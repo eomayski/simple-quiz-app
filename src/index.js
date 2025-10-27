8baa87eb2efd04a3b027db5faec8becc233af588
eomayski <emil@omayski.com>
2025-10-27 23:34:05 +0200

    Handlebars engine setup and some structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import express, { urlencoded } from "express";
+import handlebars from 'express-handlebars';
+
 import routes from "./routes.js";
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}))
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
 
 const app = express();
 
